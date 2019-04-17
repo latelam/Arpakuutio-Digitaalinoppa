5df32763c6c111b5e2a401d516fb9729845c1d9e
Late <39554860+latelam@users.noreply.github.com>
2019-04-17 12:10:14 +0300

    Arduino tietämyksen pitämistä yllä

---
+++ Arpakuutio.ino
@@ -0,0 +1,139 @@
+const int LED1 = 13;
+const int LED2 = 12;
+const int LED3 = 11;
+const int LED4 = 10;
+const int LED5 = 9;
+const int LED6 = 8;
+const int LED7 = 7;
+#define LEDON HIGH
+#define LEDOFF LOW 
+const int NAPPI = 5;
+#define NAPPIPAINETTU LOW
+
+long randNumero;
+
+void setup() {
+
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
+  pinMode(LED5, OUTPUT);
+  pinMode(LED6, OUTPUT);
+  pinMode(LED7, OUTPUT);
+  pinMode(NAPPI, INPUT_PULLUP);
+  
+  Serial.begin(9600);
+
+
+  randomSeed(analogRead(0));
+}
+
+void loop() {
+                     
+  int tila = digitalRead(NAPPI);
+  
+  if (tila == NAPPIPAINETTU) {
+  randNumero = random(1, 7);                  // tulostaa numeron 1:stä 6:seen.
+  Serial.print("Noppa: ");
+  Serial.println(randNumero);
+  noppa();
+    
+  }
+  
+}
+
+void yksi() {
+
+  digitalWrite(LED1, LEDON);
+  digitalWrite(LED2, LEDOFF);
+  digitalWrite(LED3, LEDOFF);
+  digitalWrite(LED4, LEDOFF);
+  digitalWrite(LED5, LEDOFF);
+  digitalWrite(LED6, LEDOFF);
+  digitalWrite(LED7, LEDOFF);
+}
+
+void kaksi() {
+
+  digitalWrite(LED1, LEDOFF);
+  digitalWrite(LED2, LEDON);
+  digitalWrite(LED3, LEDOFF);
+  digitalWrite(LED4, LEDOFF);
+  digitalWrite(LED5, LEDOFF);
+  digitalWrite(LED6, LEDOFF);
+  digitalWrite(LED7, LEDON);
+}
+
+void kolme() {
+
+  digitalWrite(LED1, LEDON);
+  digitalWrite(LED2, LEDON);
+  digitalWrite(LED3, LEDOFF);
+  digitalWrite(LED4, LEDOFF);
+  digitalWrite(LED5, LEDOFF);
+  digitalWrite(LED6, LEDOFF);
+  digitalWrite(LED7, LEDON);
+}
+
+void nelja() {
+
+  digitalWrite(LED1, LEDOFF);
+  digitalWrite(LED2, LEDON);
+  digitalWrite(LED3, LEDOFF);
+  digitalWrite(LED4, LEDON);
+  digitalWrite(LED5, LEDON);
+  digitalWrite(LED6, LEDOFF);
+  digitalWrite(LED7, LEDON);
+}
+
+void viisi() {
+
+  digitalWrite(LED1, LEDON);
+  digitalWrite(LED2, LEDON);
+  digitalWrite(LED3, LEDOFF);
+  digitalWrite(LED4, LEDON);
+  digitalWrite(LED5, LEDON);
+  digitalWrite(LED6, LEDOFF);
+  digitalWrite(LED7, LEDON);
+}
+
+void kuusi() {
+
+  digitalWrite(LED1, LEDOFF);
+  digitalWrite(LED2, LEDON);
+  digitalWrite(LED3, LEDON);
+  digitalWrite(LED4, LEDON);
+  digitalWrite(LED5, LEDON);
+  digitalWrite(LED6, LEDON);
+  digitalWrite(LED7, LEDON);
+}
+
+void alloff() {
+
+  digitalWrite(LED1, LEDOFF);
+  digitalWrite(LED2, LEDOFF);
+  digitalWrite(LED3, LEDOFF);
+  digitalWrite(LED4, LEDOFF);
+  digitalWrite(LED5, LEDOFF);
+  digitalWrite(LED6, LEDOFF);
+  digitalWrite(LED7, LEDOFF);
+}
+
+
+void noppa() {
+
+  if (randNumero == 1)
+  yksi();
+  else if (randNumero == 2)
+  kaksi();
+  else if (randNumero == 3)
+  kolme();
+  else if (randNumero == 4)
+  nelja();
+  else if (randNumero == 5)
+  viisi();
+  else if (randNumero == 6)
+  kuusi();
+  else alloff();
+}
